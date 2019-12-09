fe139fbc57af9535c07d3a9bddd7532d22db5648
Christoph Hitz <christoph.hitz@fhsstudents.ch>
2019-12-09 11:03:17 +0100

    little changes

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,9 @@ router.get('/', function (req, res, next) {
 router.get('/history', function (req, res, next) {
   res.send(chatHistory);
 });
+router.get('/historyamount', function (req, res, next) {
+  res.send(chatHistory.length);
+});
 router.post('/history', function (req, res, next) {
   vardate = new Date();
   chatHistory.push({username: req.body.username, content: req.body.content, timestamp: req.body.timestamp, color: req.body.color}); res.json({ message: 'Historycreated!' });
