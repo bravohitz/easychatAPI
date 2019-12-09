18d0ac0f5d5488c8888ed860696149c5a8827024
Christoph Hitz <christoph.hitz@fhsstudents.ch>
2019-12-09 11:27:20 +0100

    little test

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var chatHistory = [];
 var nicknames = [];
-
+var msgamount = 0;
 
 router.get('/', function (req, res, next) {
   res.json({ message: 'fhschat-appapiworks...' });
@@ -11,11 +11,12 @@ router.get('/history', function (req, res, next) {
   res.send(chatHistory);
 });
 router.get('/historyamount', function (req, res, next) {
-  res.send(chatHistory.length);
+  msgamount = chatHistory.length
+  res.send(msgamount);
 });
 router.post('/history', function (req, res, next) {
   vardate = new Date();
-  chatHistory.push({username: req.body.username, content: req.body.content, timestamp: req.body.timestamp, color: req.body.color}); res.json({ message: 'Historycreated!' });
+  chatHistory.push({ username: req.body.username, content: req.body.content, timestamp: req.body.timestamp, color: req.body.color }); res.json({ message: 'Historycreated!' });
 });
 
 module.exports = router;
