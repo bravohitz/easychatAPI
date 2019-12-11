122e03b5e690af943ac27dc661116de73b0e0fd5
Christoph Hitz <christoph.hitz@fhsstudents.ch>
2019-12-11 20:00:41 +0100

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var allowedOrigins = ['http://localhost:4200',
-  'https://easychatgroup4.herokuapp.com/'];
+  'http://easychatgroup4.herokuapp.com/'];
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
